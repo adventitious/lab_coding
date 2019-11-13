1ea8c0a5e7cbbcbe0a413b204c5d6772e97b75c0
name <name@lab>
2019-11-13 13:09:16 +0000

    hey

--- LabSheet6/LabSheet6/MainWindow.xaml.cs
+++ LabSheet6/LabSheet6/MainWindow.xaml.cs
@@ -20,9 +20,37 @@ namespace LabSheet6
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random r;
         public MainWindow()
         {
             InitializeComponent();
+            r = new Random();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            int r1 = r.Next(1, 21);
+            TxbInitial.Text = r1 + "";
+        }
+
+        private void BtnGuess_Click(object sender, RoutedEventArgs e)
+        {
+            int r1 = int.Parse(TxbInitial.Text);
+            int r2 = r.Next(1, 21);
+            TxbSecond.Text = r2 + "";
+
+            if (RdBig.IsChecked == true && (r2 > r1))
+            {
+                TxbWin.Text = "you win";
+            }
+            else if (RdSmall.IsChecked == true && (r2 < r1))
+            {
+                TxbWin.Text = "you win";
+            }
+            else
+            {
+                TxbWin.Text = "you lose";
+            }
         }
     }
 }
