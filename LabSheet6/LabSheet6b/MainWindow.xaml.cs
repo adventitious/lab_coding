dbce49d1cea2194502b1e5d28b71ab2e9e575090
name <name@lab>
2019-11-13 13:25:00 +0000

    hey

--- LabSheet6/LabSheet6b/MainWindow.xaml.cs
+++ LabSheet6/LabSheet6b/MainWindow.xaml.cs
@@ -24,5 +24,34 @@ namespace LabSheet6b
         {
             InitializeComponent();
         }
+
+        private void BtnOddEven_Click(object sender, RoutedEventArgs e)
+        {
+            string s1 = TxBxNumber.Text;
+            int number = 0;
+            bool isInt = int.TryParse(s1, out number);
+
+            if( isInt )
+            {
+                if( number % 2 == 0 )
+                {
+                    TxBlResult.Text = "is even";
+                    TxBlResult.Foreground = Brushes.Red;
+                    TxBlResult.Background = Brushes.GreenYellow;
+                }
+                else
+                {
+                    TxBlResult.Text = "is odd";
+                    TxBlResult.Foreground = Brushes.GreenYellow;
+                    TxBlResult.Background = Brushes.Black;
+                }
+            }
+            else
+            {
+                TxBlResult.Text = "is not int";
+                TxBlResult.Foreground = Brushes.White;
+                TxBlResult.Background = Brushes.Red;
+            }
+        }
     }
 }
