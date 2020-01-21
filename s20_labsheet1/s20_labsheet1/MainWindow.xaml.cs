cb9de9b5d47c49cc3f2c2985b08f16501a0eae2a
s00190028 <s00190028@LAB-B1042-11>
2020-01-21 12:58:58 +0000

    s

--- s20_labsheet1/s20_labsheet1/MainWindow.xaml.cs
+++ s20_labsheet1/s20_labsheet1/MainWindow.xaml.cs
@@ -23,6 +23,28 @@ namespace s20_labsheet1
         public MainWindow()
         {
             InitializeComponent();
+            Band[] bands = new Band[6];
+
+            bands[0] = new Band("The Beatles", 1960, new string[] { "John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr" } );
+            bands[1] = new Band("The Monkees", 1966, new string[] { "Micky Dolenz", "Michael Nesmith", "Peter Tork", "Davy Jones" } );
+            bands[2] = new Band("Queen", 1970, new string[] { "Freddie Mercury", "Brian May", "John Deacon", "Roger Taylor" } );
+
+            bands[3] = new Band("U2", 1976, new string[] { "Bono", "the Edge", "Adam Clayton", "Larry Mullen Jr" } );
+            bands[4] = new Band("The Black Keys", 2001, new string[] { "Dan Auerbach", "Patrick Carney" } );
+            bands[5] = new Band("Motörhead", 1975, new string[] { "Lemmy", "Larry Wallis", "Lucas Fox", "Phil Taylor", "Eddie Clarke" } );
+
+        }
+    }
+
+    class Band
+    {
+        string bandName;
+        int yearFormed;
+        string[] Members;
+
+        public Band( string BandName, int YearFormed, string[] Members )
+        {
+
         }
     }
 }
