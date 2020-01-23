f7909ba5f420d04850ac19c5e3de3821542add2a
sss <s@sss>
2020-01-23 17:55:54 +0000

    s

--- s20_labsheet1/s20_labsheet1/MainWindow.xaml.cs
+++ s20_labsheet1/s20_labsheet1/MainWindow.xaml.cs
@@ -25,52 +25,127 @@ namespace s20_labsheet1
         public MainWindow()
         {
             InitializeComponent();
-            /*
-            Band[] bands = new Band[6];
-
-            bands[0] = new Band("The Beatles", 1960, new string[] { "John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr" } );
-            bands[1] = new Band("The Monkees", 1966, new string[] { "Micky Dolenz", "Michael Nesmith", "Peter Tork", "Davy Jones" } );
-            bands[2] = new Band("Queen", 1970, new string[] { "Freddie Mercury", "Brian May", "John Deacon", "Roger Taylor" } );
-
-            bands[3] = new Band("U2", 1976, new string[] { "Bono", "the Edge", "Adam Clayton", "Larry Mullen Jr" } );
-            bands[4] = new Band("The Black Keys", 2001, new string[] { "Dan Auerbach", "Patrick Carney" } );
-            bands[5] = new Band("Motörhead", 1975, new string[] { "Lemmy", "Larry Wallis", "Lucas Fox", "Phil Taylor", "Eddie Clarke" } );
-            */
         }
 
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             bands = new List<Band>();
-            /*
-            bands.Add(new RockBand("The Beatles", 1960, new string[] { "John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr" }));
-            bands.Add(new RockBand("The Monkees", 1966, new string[] { "Micky Dolenz", "Michael Nesmith", "Peter Tork", "Davy Jones" }));
-            bands.Add(new RockBand("Queen", 1970, new string[] { "Freddie Mercury", "Brian May", "John Deacon", "Roger Taylor" }));
-            bands.Add(new RockBand("U2", 1976, new string[] { "Bono", "the Edge", "Adam Clayton", "Larry Mullen Jr" }));  
-            bands.Add(new RockBand("The Black Keys", 2001, new string[] { "Dan Auerbach", "Patrick Carney" }));
-            bands.Add(new RockBand("Motörhead", 1975, new string[] { "Lemmy", "Larry Wallis", "Lucas Fox", "Phil Taylor", "Eddie Clarke" }));
-            */
-            Lsb_Bands.ItemsSource = bands;
-            bands.Sort();
-            Lsb_Bands.Items.Refresh();
+            //          bands.Add(new RockBand(bandName:  "The Beatles", yearFormed: 1960, members: new string[] { "John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr" }));
 
+            Band tempBand = new RockBand("The Beatles", 1960, new string[] { "John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr" });
+            tempBand.AddAlbum(new Album("white album 1968"));
+            tempBand.AddAlbum(new Album("revolver 1966"));
+
+            bands.Add(tempBand);
+
+            tempBand = new PopBand("The Monkees", 1966, new string[] { "Micky Dolenz", "Michael Nesmith", "Peter Tork", "Davy Jones" });
+            tempBand.AddAlbum(new Album("More of The Monkees 67"));
+            tempBand.AddAlbum(new Album("Instant Replay(1969)"));
+
+            bands.Add(tempBand);
+
+            tempBand = new RockBand("Queen", 1970, new string[] { "Freddie Mercury", "Brian May", "John Deacon", "Roger Taylor" });
+            tempBand.AddAlbum(new Album("Jazz(1978)"));
+            tempBand.AddAlbum(new Album("A Kind of Magic (1986)"));
+
+            bands.Add(tempBand);
+
+            tempBand = new RockBand("U2", 1976, new string[] { "Bono", "the Edge", "Adam Clayton", "Larry Mullen Jr" });
+            tempBand.AddAlbum(new Album("Boy 1980"));
+            tempBand.AddAlbum(new Album("Rattle and Hum 1988"));
+
+            bands.Add(tempBand);
+
+            tempBand = new IndyBand("The Black Keys", 2001, new string[] { "Dan Auerbach", "Patrick Carney" });
+            tempBand.AddAlbum(new Album("Thickfreakness 2003"));
+            tempBand.AddAlbum(new Album("El Camino 2011"));
+
+            bands.Add(tempBand);
+
+            tempBand = new RockBand("Motörhead", 1975, new string[] { "Lemmy", "Larry Wallis", "Lucas Fox", "Phil Taylor", "Eddie Clarke" });
+            tempBand.AddAlbum(new Album("Ace of Spades (1980)"));
+            tempBand.AddAlbum(new Album("Iron Fist (1982)"));
+
+            bands.Add(tempBand);
+
+            bands.Sort();
+
+            Lsb_Bands.ItemsSource = bands;
+            Lsb_Bands.Items.Refresh();
         }
 
+        private void Lsb_Bands_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Lsb_Bands.SelectedItem != null)
+            {
+                Band band = (Band)Lsb_Bands.SelectedItem;
+                Lsb_Albums.ItemsSource = band.Albums;
+                Lsb_Albums.Items.Refresh();
+
+                Txbk_YearFormed.Text = band.YearFormed.ToString();
+                Txbk_Members.Text = band.MembersString();
+
+                //string msgtext = "That date is not free"+ band.Albums[0].ToString();
+                //MessageBoxResult result = MessageBox.Show(msgtext, "Message", MessageBoxButton.OK);
+
+            }
+        }
     }
 
-    abstract class Band : IComparable<Band>
+    public class Album
+    {
+        public string AlbumName { get; set; }
+        public int YearRelease { get; set; }
+        public int Sales { get; set; }
+
+        public Album( string albumName )
+        {
+            AlbumName = albumName;
+        }
+
+        public override string ToString()
+        {
+            return string.Format( AlbumName );
+        }
+    }
+
+
+    public abstract class Band : IComparable<Band>
     {
         public string BandName { get; set; }
         public int YearFormed { get; set; }
         public string[] Members { get; set; }
+
+
+        public List<Album> Albums { get; set; }
 
         public Band(string bandName, int yearFormed, string[] members)
         {
             BandName = bandName;
             YearFormed = yearFormed;
             Members = members;
+            Albums = new List<Album>();
         }
 
+
+        public string MembersString()
+        {
+            string out1 = "";
+            foreach (var member in Members )
+            {
+                out1 += member + ", ";
+            }
+
+            out1 = out1.Remove(out1.Length - 2);
+
+            return out1;
+        }
+
+        public void AddAlbum( Album album )
+        {
+            Albums.Add(album);
+        }
 
         public override string ToString()
         {
@@ -83,16 +158,39 @@ namespace s20_labsheet1
         }
     }
 
-    class RockBand : Band, IComparable<Band>
+    public class RockBand : Band
     {
-        public string BandName { get; set; }
-        public int YearFormed { get; set; }
-        public string[] Members { get; set; }
-
-        public RockBand( string bandName )
+        public RockBand(string bandName, int yearFormed, string[] members) : base(bandName, yearFormed, members)
         {
-
         }
 
+        public override string ToString()
+        {
+            return string.Format(this.BandName + " - Rock");
+        }
+    }
+
+    public class PopBand : Band
+    {
+        public PopBand(string bandName, int yearFormed, string[] members) : base(bandName, yearFormed, members)
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Format(this.BandName + " - Pop");
+        }
+    }
+
+    public class IndyBand : Band
+    {
+        public IndyBand(string bandName, int yearFormed, string[] members) : base(bandName, yearFormed, members)
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Format(this.BandName + " - Indy");
+        }
     }
 }
