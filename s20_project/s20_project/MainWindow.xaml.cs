f28a860f824f097029257cade5c3d24bf8595b0e
s <s>
2020-01-29 09:55:42 +0000

    s

---
+++ s20_project/s20_project/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace s20_project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Btn_NewContest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_RemoveCandidate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public class Contest
+    {
+    }
+    public class Candidate
+    {
+    }
+    public class BallotPaper
+    {
+    }
+
+    public class SimpleCount //
+    {
+    }
+
+
+}
