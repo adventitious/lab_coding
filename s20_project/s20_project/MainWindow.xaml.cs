a82f33f24f4e161f9eca3f56e0cd81ce30f82b91
s <k@b.k>
2020-02-05 20:08:11 +0000

    s

--- s20_project/s20_project/MainWindow.xaml.cs
+++ s20_project/s20_project/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace s20_project
     {
         public Contest ContestCurrent;
 
+        Random r = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,10 +31,18 @@ namespace s20_project
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            //MakeNewContest();
         }
 
         private void Btn_NewContest_Click(object sender, RoutedEventArgs e)
+        {
+            MakeNewContest3();
+
+            doSimpleCount();
+        }
+
+
+        private void MakeNewContest1()   // simple pr
         {
             ContestCurrent = new Contest();
 
@@ -84,8 +94,107 @@ namespace s20_project
             Lsb_Votes.Items.Refresh();
         }
 
+
+
+        private void MakeNewContest2()   // simple fpp
+        {
+            ContestCurrent = new Contest();
+
+            ContestCurrent.AddCandidate(new Candidate("John"));
+            ContestCurrent.AddCandidate(new Candidate("Mary"));
+            ContestCurrent.AddCandidate(new Candidate("Dan"));
+
+            Lsb_Candidates.ItemsSource = ContestCurrent.Candidates;
+            Lsb_Candidates.Items.Refresh();
+
+
+            BallotPaper b;
+            b = new BallotPaper();
+            b.AddVote(new Vote(ContestCurrent.Candidates[0], 1));
+
+            ContestCurrent.AddBallotPaper(b);
+
+            b = new BallotPaper();
+            b.AddVote(new Vote(ContestCurrent.Candidates[0], 1));
+
+            ContestCurrent.AddBallotPaper(b);
+
+            b = new BallotPaper();
+            b.AddVote(new Vote(ContestCurrent.Candidates[2], 1));
+
+            ContestCurrent.AddBallotPaper(b);
+
+            b = new BallotPaper();
+            b.AddVote(new Vote(ContestCurrent.Candidates[2], 1));
+
+            ContestCurrent.AddBallotPaper(b);
+
+            b = new BallotPaper();
+            b.AddVote(new Vote(ContestCurrent.Candidates[1], 1));
+            ContestCurrent.AddBallotPaper(b);
+
+            Lsb_Votes.ItemsSource = ContestCurrent.BallotPapers;
+            Lsb_Votes.Items.Refresh();
+        }
+
+
+        private void MakeNewContest3()   // simple fpp
+        {
+            ContestCurrent = new Contest();
+
+            ContestCurrent.AddCandidate(new Candidate("John"));
+            ContestCurrent.AddCandidate(new Candidate("Mary"));
+            ContestCurrent.AddCandidate(new Candidate("Dan"));
+
+            Lsb_Candidates.ItemsSource = ContestCurrent.Candidates;
+            Lsb_Candidates.Items.Refresh();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                BallotPaper b;
+                b = new BallotPaper();
+                int rint = r.Next(0, 3 );
+                //b.AddVote(new Vote(ContestCurrent.Candidates[r.Next(0, rint )], 1));
+                b.AddVote(new Vote(ContestCurrent.Candidates[ rint ], 1));
+
+                ContestCurrent.AddBallotPaper(b);
+            }
+
+
+            Lsb_Votes.ItemsSource = ContestCurrent.BallotPapers;
+            Lsb_Votes.Items.Refresh();
+        }
+
+
+
         private void Btn_RemoveCandidate_Click(object sender, RoutedEventArgs e)
         {
+            int rint = r.Next(0, 3);
+            MessageBox.Show("You said: " + " qqq: " + rint);
+        }
+
+
+
+        private void Btn_Count_Click(object sender, RoutedEventArgs e)
+        {
+            doSimpleCount();
+        }
+
+
+        private void doSimpleCount()
+        {
+            try
+            {
+                SimpleCount sc = new SimpleCount(ContestCurrent.Candidates, ContestCurrent.BallotPapers);
+
+                string s = sc.getResults();
+                Txb_Results.Text = s;
+            }
+            catch ( Exception exc)
+            {
+                MessageBox.Show("You said: " + " wwww: " + exc.Message);
+            }
         }
     }
 
@@ -121,6 +230,7 @@ namespace s20_project
     public class Candidate
     {
         public string CandidateName { get; set; }
+        public int VotesReceived { get; set; }
         public Candidate( string candidateName )
         {
             CandidateName = candidateName;
@@ -129,6 +239,12 @@ namespace s20_project
         public override string ToString()
         {
             return CandidateName;
+        }
+
+        public void gotAVote()
+        {
+            //simple so far
+            VotesReceived++;
         }
     }
 
@@ -151,10 +267,68 @@ namespace s20_project
             out1 = out1.Remove(out1.Length - 2);
             return out1;
         }
+
+        public Candidate getPreferenceOfInt( int preference )
+        {
+            foreach (Vote vote in Votes)
+            {
+                if( vote.Preference == preference )
+                {
+                    return vote.Candidate;
+                }
+            }
+            return null;
+        }
     }
 
     public class SimpleCount
     {
+        public List<Candidate> Candidates;
+        public List<BallotPaper> BallotPapers;
+
+        public SimpleCount( List<Candidate> candidates, List<BallotPaper> ballotPapers )
+        {
+
+            Candidates = candidates;
+            BallotPapers = ballotPapers;
+        }
+
+        public string getResults()
+        {
+            string out1 = "";
+            int[] counts = new int[Candidates.Count()];
+            foreach( BallotPaper bp in BallotPapers )
+            {
+                Candidate gotVote = bp.getPreferenceOfInt(1);
+                gotVote.gotAVote();
+            }
+            List<Candidate> tieWinners = new List<Candidate>();
+
+            int highestC = 0;
+
+            foreach (Candidate c in Candidates)
+            {
+                if (c.VotesReceived == highestC)
+                {
+                    tieWinners.Add(c);
+                }
+                if (c.VotesReceived > highestC)
+                {
+                    tieWinners.Clear();
+                    tieWinners.Add(c);
+                    highestC = c.VotesReceived;
+                }
+            }
+
+            foreach (Candidate c in tieWinners )
+            {
+                out1 += c.CandidateName + " got " + c.VotesReceived + '\n';
+            }
+            //out1 += highestC.CandidateName + " got " + highestC.VotesReceived + '\n';
+
+
+            return out1;
+        }
     }
 
 
