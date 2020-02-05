7aac5376c71d56c905dd6575cd78cbd9b5a8cd84
s00190028 <s00190028@LAB-B1042-11>
2020-02-05 09:57:30 +0000

    s

---
+++ s20_LabSheet2/Exercise1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberLambda();
+            NumbersQuery();
+        }
+        static void NumberLambda()
+        {
+            int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+            var outputNumbers = numbers.Where(n => n > 5);
+
+            foreach (int number in outputNumbers)
+            {
+                Console.WriteLine(number.ToString());
+            }
+            Console.WriteLine( numbers.Length );
+            Console.WriteLine( "output count:" + outputNumbers.Count() + "\n");
+        }
+
+        static void NumbersQuery()
+        {
+            int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+            int count = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number > 5)
+                {
+                    count++;
+                }
+            }
+
+            int[] outputNumbers = new int[count];
+            count = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number > 5)
+                {
+                    outputNumbers[count] = number;
+                    count++;
+                }
+            }
+
+            foreach (int number in outputNumbers)
+            {
+                Console.WriteLine(number.ToString());
+            }
+            Console.WriteLine("output count:" + outputNumbers.Count());
+        }
+    }
+}
---
+++ s20_LabSheet2/Exercise2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worldx!");
+            q2b();
+        }
+
+        static void q2()
+        {
+            var files = new DirectoryInfo("C:\\Windows").GetFiles();
+
+            var query = from item in files
+                        where item.Length > 10000
+                        orderby item.Length, item.Name
+                        select new MyFileInfo
+                        {
+                            Name = item.Name,
+                            Length = item.Length,
+                            CreationTime = item.CreationTime
+                        };
+            Console.WriteLine("Filename\tSize\tCreationDate");
+
+            foreach( var item in query )
+            {
+                Console.WriteLine("{0} \t{1} bytes,\t{2}", item.Name, item.Length, item.CreationTime);
+            }
+            Console.WriteLine();
+        }
+
+        static void q2b()
+        {
+            var files = new DirectoryInfo("C:\\Windows").GetFiles();
+
+            var query = files
+                .Where(f => f.Length > 10000)
+                .OrderBy(f => f.Length).ThenBy(f => f.Name)
+                .Select(f => new MyFileInfo
+                {
+                    Name = f.Name,
+                    Length = f.Length,
+                    CreationTime = f.CreationTime
+                } );
+
+            Console.WriteLine("Filename\tSize\tCreationDate");
+
+            foreach (var item in query)
+            {
+                Console.WriteLine("{0} \t{1} bytes,\t{2}", item.Name, item.Length, item.CreationTime);
+            }
+        }
+    }
+
+    public class MyFileInfo
+    {
+        public string Name{ get; set;}
+        public long Length{ get; set;}
+        public DateTime CreationTime{ get; set;}
+    }
+}
