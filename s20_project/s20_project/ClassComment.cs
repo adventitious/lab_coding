0c641b58169fc6585b182e570e88fe024e01933f
sss <s@sss>
2020-02-26 09:45:41 +0000

    s

---
+++ s20_project/s20_project/ClassComment.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s20_project
+{
+    class ClassComment
+    {
+        /*
+        List<Candidate> tieWinners = new List<Candidate>();
+
+        int highestC = 0;
+
+        foreach (Candidate c in Candidates)
+        {
+            if (c.VotesReceived == highestC)
+            {
+                tieWinners.Add(c);
+            }
+            if (c.VotesReceived > highestC)
+            {
+                tieWinners.Clear();
+                tieWinners.Add(c);
+                highestC = c.VotesReceived;
+            }
+        }
+
+        foreach (Candidate c in tieWinners )
+        {
+            out1 += c.CandidateName + " got " + c.VotesReceived + '\n';
+        }
+        */
+
+    }
+
+
+    public class Vote : IComparable<Vote>
+    {
+        public Candidate Candidate { get; set; }
+        public int Preference { get; set; }
+
+        public Vote(Candidate candidate, int preference)
+        {
+            Candidate = candidate;
+            Preference = preference;
+        }
+
+        public int CompareTo(Vote that)
+        {
+            return -that.Preference.CompareTo(Preference);
+        }
+    }
+
+
+
+    public class Candidate : IComparable<Candidate>
+    {
+        public string CandidateName { get; set; }
+        //public int VotesReceived { get; set; }
+        public double VotesReceived { get; set; }
+        public Candidate(string candidateName)
+        {
+            CandidateName = candidateName;
+        }
+
+        public override string ToString()
+        {
+            return CandidateName;
+        }
+
+        public void gotAVote(double voteValue)
+        {
+            VotesReceived += voteValue;
+        }
+
+        public int CompareTo(Candidate that)
+        {
+            return that.VotesReceived.CompareTo(VotesReceived);
+        }
+    }
+
+    public class BallotPaper : IComparable<BallotPaper>
+    {
+        public List<Vote> Votes = new List<Vote>();
+
+        public void AddVote(Vote vote)
+        {
+            Votes.Add(vote);
+        }
+
+        public override string ToString()
+        {
+            string out1 = "";
+            Votes.Sort();
+            foreach (var vote in Votes)
+            {
+                out1 += vote.Candidate.CandidateName + ": " + vote.Preference + ", ";
+            }
+            out1 = out1.Remove(out1.Length - 2);
+            return out1;
+        }
+
+        public Candidate getPreferenceOfInt(int preference)
+        {
+            foreach (Vote vote in Votes)
+            {
+                if (vote.Preference == preference)
+                {
+                    return vote.Candidate;
+                }
+            }
+            return null;
+        }
+        public int CompareTo(BallotPaper that)
+        {
+            for (int i = 0; i < Votes.Count(); i++)
+            {
+                if (i < that.Votes.Count())
+                {
+                    int sortInt = that.Votes[i].Candidate.CandidateName.CompareTo(Votes[i].Candidate.CandidateName);
+
+                    if (sortInt != 0)
+                    {
+                        return -sortInt;
+                    }
+                }
+            }
+            /*
+            if( sortInt != 0 )
+            {
+                return 0;
+            }
+            */
+            return 0;
+        }
+    }
+
+
+
+}
--- s20_project/s20_project/MainWindow.xaml.cs
+++ s20_project/s20_project/MainWindow.xaml.cs
@@ -56,7 +56,6 @@ namespace s20_project
             Lsb_Candidates.ItemsSource = ContestCurrent.Candidates;
             Lsb_Candidates.Items.Refresh();
 
-
             BallotPaper b;
             b = new BallotPaper();
             b.AddVote(new Vote(ContestCurrent.Candidates[0], 1));
@@ -289,107 +288,6 @@ namespace s20_project
         }
     }
 
-    public class Vote : IComparable<Vote>
-    {
-        public Candidate Candidate{get; set; }
-        public int Preference{get; set; }
-
-        public Vote( Candidate candidate, int preference )
-        {
-            Candidate = candidate;
-            Preference = preference;
-        }
-
-        public int CompareTo(Vote that)
-        {
-            return -that.Preference.CompareTo(Preference);
-        }
-    }
-
-
-
-    public class Candidate : IComparable<Candidate>
-    {
-        public string CandidateName { get; set; }
-        //public int VotesReceived { get; set; }
-        public double VotesReceived { get; set; }
-        public Candidate( string candidateName )
-        {
-            CandidateName = candidateName;
-        }
-
-        public override string ToString()
-        {
-            return CandidateName;
-        }
-
-        public void gotAVote( double voteValue )
-        {
-            VotesReceived += voteValue;
-        }
-
-        public int CompareTo(Candidate that)
-        {
-            return that.VotesReceived.CompareTo(VotesReceived);
-        }
-    }
-
-    public class BallotPaper : IComparable<BallotPaper>
-    {
-        public List<Vote> Votes = new List<Vote>();
-
-        public void AddVote(Vote vote)
-        {
-            Votes.Add(vote);
-        }
-
-        public override string ToString()
-        {
-            string out1 = "";
-            Votes.Sort();
-            foreach (var vote in Votes)
-            {
-                out1 += vote.Candidate.CandidateName + ": " + vote.Preference + ", ";
-            }
-            out1 = out1.Remove(out1.Length - 2);
-            return out1;
-        }
-
-        public Candidate getPreferenceOfInt( int preference )
-        {
-            foreach (Vote vote in Votes)
-            {
-                if( vote.Preference == preference )
-                {
-                    return vote.Candidate;
-                }
-            }
-            return null;
-        }
-        public int CompareTo(BallotPaper that)
-        {
-            for (int i = 0; i < Votes.Count(); i++)
-            {
-                if( i < that.Votes.Count() )
-                {
-                    int sortInt = that.Votes[i].Candidate.CandidateName.CompareTo(Votes[i].Candidate.CandidateName);
-
-                    if (sortInt != 0)
-                    {
-                        return -sortInt;
-                    }
-                }
-            }
-            /*
-            if( sortInt != 0 )
-            {
-                return 0;
-            }
-            */
-            return 0;
-        }
-    }
-
     public class SimpleCount
     {
         public List<Candidate> Candidates;
@@ -401,44 +299,106 @@ namespace s20_project
             Candidates = candidates;
             BallotPapers = ballotPapers;
 
-            /*
-            List<Candidate> tieWinners = new List<Candidate>();
-
-            int highestC = 0;
-
-            foreach (Candidate c in Candidates)
-            {
-                if (c.VotesReceived == highestC)
-                {
-                    tieWinners.Add(c);
-                }
-                if (c.VotesReceived > highestC)
-                {
-                    tieWinners.Clear();
-                    tieWinners.Add(c);
-                    highestC = c.VotesReceived;
-                }
-            }
-
-            foreach (Candidate c in tieWinners )
-            {
-                out1 += c.CandidateName + " got " + c.VotesReceived + '\n';
-            }
-            */
-
 
         }
 
         public string getResults()
         {
             string out1 = "";
-            int[] counts = new int[Candidates.Count()];
-            foreach( BallotPaper bp in BallotPapers )
+
+            // seats : 2
+            // total valid vote : 10
+
+            // step 1 
+            // quota
+            // "The Quota is determined by dividing the total valid vote 
+            // by one more than the number of places to be filled,
+            // continuing the calculation to two decimal places, and rounding up."
+
+            // 10 / 3 = 3.33 --> 3.34
+
+
+
+            double seats = 2;
+            double quota = BallotPapers.Count() / (seats + 1);
+
+            quota = Math.Round(quota, 2); // ...round up some
+
+            out1 += "quota: " + quota.ToString() + "\n\n";             // display the quota
+
+
+
+            foreach (BallotPaper bp in BallotPapers)
             {
                 Candidate gotVote = bp.getPreferenceOfInt(1);
-                gotVote.gotAVote( 1.0 );
+                gotVote.gotAVote(1.0);
             }
-            
+            out1 += ListCanVotes();                            // display the candidates and their votes 
+            out1 += "\n";
+
+
+
+            // if a candidate has more than the quota, that candidate is elected
+
+            // else, the candidate with least votes is removed
+
+            // candidate with a vote of that exceeds the quota is deemed elected.
+            List<Candidate> elected = new List<Candidate>();
+            foreach (Candidate c in Candidates)
+            {
+                if (c.VotesReceived > quota)
+                {
+                    out1 += c.CandidateName + " is elected with a surplus of " + (c.VotesReceived - quota);
+                    out1 += "\n";
+                    elected.Add(c);
+                }
+            }
+
+
+
+
+            // Step 2
+            // if surplus is greater than the difference between the votes of the last two candidates,
+            // the surplus must be transferred.
+            // if it is less it won't make any difference, so do anyway...
+
+            // elected got 5 votes, all fully filled in 
+            // surplus: 1.67
+            // 1.67 / 5 = .33
+            // transfervalue = 0.33
+
+            // how many of the electeds votes are transferrable, 
+            // if their 2nd preference is elected,    not transferrable
+            // if their 2nd preference is eliminated, not transferrable
+            // if their 2nd preference is empty,      not transferrable
+
+
+
+
+            foreach (Candidate c in elected)
+            {
+                out1 += DistributeCandidatesSurplus(c, BallotPapers, quota, elected, out1);
+                out1 += "\n";
+            }
+
+            out1 += "\n\nafter step 2\n" + ListCanVotes();
+
+            return out1;
+        }
+
+
+
+
+        public string getResults1()
+        {
+            string out1 = "";
+            int[] counts = new int[Candidates.Count()];
+            foreach (BallotPaper bp in BallotPapers)
+            {
+                Candidate gotVote = bp.getPreferenceOfInt(1);
+                gotVote.gotAVote(1.0);
+            }
+
             // seats : 2
             // total valid vote : 10
 
@@ -452,7 +412,7 @@ namespace s20_project
 
             double seats = 2;
 
-            double quota= BallotPapers.Count()  / (seats + 1);
+            double quota = BallotPapers.Count() / (seats + 1);
             quota = Math.Round(quota, 2); // round up some
 
             out1 += ListCanVotes();
@@ -463,12 +423,16 @@ namespace s20_project
             List<Candidate> elected = new List<Candidate>();
             foreach (Candidate c in Candidates)
             {
-                if (c.VotesReceived > quota )
+                if (c.VotesReceived > quota)
                 {
-                    out1 += '\n' + c.CandidateName + " is elected with a surplus of "+ (c.VotesReceived - quota);
+                    out1 += '\n' + c.CandidateName + " is elected with a surplus of " + (c.VotesReceived - quota);
                     elected.Add(c);
                 }
             }
+
+
+
+
             // Step 2
             // if surplus is greater than the difference between the votes of the last two candidates,
             // the surplus must be transferred.
@@ -478,21 +442,21 @@ namespace s20_project
             // surplus: 1.67
             // 1.67 / 5 = .33
             // transfervalue = 0.33
-            
+
             // how many of the electeds votes are transferrable, 
             // if their 2nd preference is elected,    not transferrable
             // if their 2nd preference is eliminated, not transferrable
             // if their 2nd preference is empty,      not transferrable
-            
 
 
 
-            foreach ( Candidate c in elected )
+
+            foreach (Candidate c in elected)
             {
                 out1 += "\n";
                 out1 += DistributeCandidatesSurplus(c, BallotPapers, quota, elected, out1);
             }
-            
+
             out1 += "\n\nafter step 2\n" + ListCanVotes();
 
             return out1;
